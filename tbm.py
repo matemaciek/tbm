dbd641b3bb98b61c8ce5e9ef1a7c9f88b3816289
Maciej Sokołowski <matemaciek@gmail.com>
2013-11-15 16:27:09 +0100

    passing descriptor to dispatch, preparation for umsgpack

--- tbm.py
+++ tbm.py
@@ -1,14 +1,17 @@
 # remember: sudo python tbm.py
 
-import pcap
 import eventlet
-
+import pcap
+#import umsgpack
 
 eths = ["lo", "any", "eth0"]
 devs = {}
 
-def print_packet(length, data, timestamp):
-    print "len: {0}, time: {1}, data:\n{2}\n".format(length, timestamp, data.encode("hex"))
+def print_packet(length, data, timestamp, fd):
+    message = {"length": length, "timestamp": timestamp, "from": devs[fd], "fd": fd, "data": data}
+    #print "len: {0}, time: {1}, data:\n{2}\n".format(length, timestamp, data.encode("hex"))
+    #print umsgpack.packb(message)
+    print message
 
 
 def handle(dev):
@@ -16,7 +19,7 @@ def handle(dev):
         print "? Waiting for {0} ({1})".format(devs[dev.fileno()], dev.fileno())
         eventlet.hubs.trampoline(dev.fileno(), read=True)
         print "... Reading from {0} ({1})".format(devs[dev.fileno()], dev.fileno())
-        dev.dispatch(1, print_packet)
+        dev.dispatch(0, lambda l, d, t: print_packet(l, d, t, dev.fileno()))
         print "! Read from {0} ({1})".format(devs[dev.fileno()], dev.fileno())
 
 for eth in eths:
