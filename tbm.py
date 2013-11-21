5966ae6c0a9f14bc54ba9627c69eddbe56ed1ada
Maciej Sokołowski <matemaciek@gmail.com>
2013-11-21 14:07:04 +0100

    devs storing by id

--- tbm.py
+++ tbm.py
@@ -9,28 +9,39 @@ import msgpack # msgpack-python
 #TODO: requirements.txt
 
 
-def print_packet(length, data, timestamp, fd):
+def send_packet(length, data, timestamp, dev_id):
     if length > 20000:
         print "WARNING: Large packet: {0} (sniffing loop?)".format(length)
-    message = {"ln": length, "ts": timestamp, "if": devs[fd], "fd": fd, "dt": data}
+    message = {"ln": length, "ts": timestamp, "if": devs[dev_id]["eth"], "id": dev_id, "dt": data}
     clients_copy = clients
     for client in clients_copy:
         try:
             msgpack.pack(message, client)
             client.flush()
         except eventlet.green.socket.error:
-            print "client {0} disconnected".format(client.fileno())
+            print "client disconnected (fd: {0})".format(client.fileno())
             clients.remove(client)
 
 
-def handle_eth(dev):
+def receive_packet(message):
+    if len(devs) <= message["id"]:
+        print "Nowhere to forward packet, dropping it."
+    else:
+        dev = devs[message["id"]]
+        print "time: {0}, src_interface: {1}, dst_interface: {2}, len: {3}".format(message["ts"], message["if"],
+                                                                                   dev["eth"], message["ln"])
+        assert message["ln"] == len(message["dt"]), "difference: {0}".format(message["ln"] - len(message["dt"]))
+
+
+def handle_eth(dev_id):
     while True:
+        dev = devs[dev_id]["dev"]
         eventlet.hubs.trampoline(dev.fileno(), read=True)
-        dev.dispatch(0, lambda l, d, t: print_packet(l, d, t, dev.fileno()))
+        dev.dispatch(0, lambda l, d, t: send_packet(l, d, t, dev_id))
 
 
 def handle_client_connected(fd):
-    print "client {0} connected".format(fd.fileno())
+    print "client connected (fd: {0})".format(fd.fileno())
     clients.append(fd)
 
 
@@ -46,8 +57,7 @@ def start_client(host, port):
                 break
             unpacker.feed(buf)
             for message in unpacker:
-                print "time: {1}, interface: {2}, len: {0}\n".format(message["ln"], message["ts"], message["if"])
-                assert message["ln"] == len(message["dt"]), "difference: {0}".format(message["ln"] - len(message["dt"]))
+                receive_packet(message)
     except KeyboardInterrupt:
         s.close()
 
@@ -62,24 +72,26 @@ def start_server(port):
 
 
 def open_eths(eths):
-    for (id, eth) in eths:
+    devs = []
+    for (id, eth) in enumerate(eths):
         dev = pcap.pcapObject()
         dev.open_live(eth, 65536, 1, 0)
-        devs[dev.fileno()] = (id, eth)
-        eventlet.spawn(handle_eth, dev)
+        devs.append({"dev": dev, "eth": eth})
+        eventlet.spawn(handle_eth, id)
+    return devs
+
 
 parser = argparse.ArgumentParser(description="TODO describe program")
-parser.add_argument("-c", "--client", metavar="HOST", help="run as client, connect to HOST", default=None)
+parser.add_argument("-c", "--client", metavar="HOST", help="run as client, connect to HOST", default=None, dest="host")
 parser.add_argument("-p", "--port", type=int, help="port of communication", default=6000)
 parser.add_argument("interfaces", metavar="IF", nargs="+", help="interfaces to tunnel")
 #TODO: debug / minimal mode?
 args = parser.parse_args()
 
-devs = {}
 clients = []
+devs = open_eths(args.interfaces)
 
-open_eths(enumerate(args.interfaces))
-if args.client is None:
+if args.host is None:
     start_server(args.port)
 else:
-    start_client(args.client, args.port)
+    start_client(args.host, args.port)
